ad4a1c48b3dbb1a6651d0835f4959a691bb3d5f9
NajmAlzorqah <najmalzorqah@gmail.com>
2024-06-21 05:58:53 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/react-eproject/", // Specify the base path for GitHub Pages deployment
   plugins: [react()],
   define: {
     "process.env": {},
+  },
+  build: {
+    outDir: "dist", // Specify the output directory for production build
+    assetsDir: "", // Ensure assets are correctly referenced relative to root
+    sourcemap: false, // Disable sourcemaps for production (optional, for smaller bundle size)
   },
 });
