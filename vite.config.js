c1b30f8d5671d56c39ec91bcd518ca829161fd09
NajmAlzorqah <najmalzorqah@gmail.com>
2024-09-14 19:27:27 +0300

    fixing the deployments problems

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/react-eproject/",
+  base: "/belleville-dental-react-eproject/",
   plugins: [react()],
   resolve: {
     alias: {
